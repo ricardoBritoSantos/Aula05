5d29d8dba97d7a0dc1ba1e351ac1f794a9b2bbd6
Ricardo <ricardo_brito_santos@hotmail.com>
2021-06-14 21:03:49 -0300

    Corrigido o bug da mensagem que ocacionamente não aparecia

--- src/TeacherHelper.java
+++ src/TeacherHelper.java
@@ -58,6 +58,9 @@ public class TeacherHelper {
 			case 3:
 				
 				switch(gerador.nextInt(7)) {
+				case 0:
+					System.out.println("Faça ou não faça, tentativa não há");
+					break;
 				case 1:
 					System.out.println("Você é um professor incrível!");
 					break;
